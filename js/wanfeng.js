5e479f1dd07fbef3691508a74124923a60c4b99e
oilycn <oxmosama@outlook.com>
2022-08-25 21:28:11 +0800

    Site updated: 2022-08-25 21:28:10

--- js/wanfeng.js
+++ js/wanfeng.js
@@ -1,1 +1,67 @@
-"use strict";function qipao(){$("#page-header").circleMagic({radius:10,density:.2,color:"rgba(255,255,255,.4)",clearOffset:.99})}!function(i){i.fn.circleMagic=function(t){var o,n,a,r,e=!0,c=[],l=i.extend({color:"rgba(255,0,0,.5)",radius:10,density:.3,clearOffset:.2},t),d=this[0];function s(){if(e)for(var t in r.clearRect(0,0,o,n),c)c[t].draw();requestAnimationFrame(s)}function h(){var t=this;function e(){t.pos.x=Math.random()*o,t.pos.y=n+100*Math.random(),t.alpha=.1+Math.random()*l.clearOffset,t.scale=.1+.3*Math.random(),t.speed=Math.random(),"random"===l.color?t.color="rgba("+Math.floor(255*Math.random())+", "+Math.floor(0*Math.random())+", "+Math.floor(0*Math.random())+", "+Math.random().toPrecision(2)+")":t.color=l.color}t.pos={},e(),this.draw=function(){t.alpha<=0&&e(),t.pos.y-=t.speed,t.alpha-=5e-4,r.beginPath(),r.arc(t.pos.x,t.pos.y,t.scale*l.radius,0,2*Math.PI,!1),r.fillStyle=t.color,r.fill(),r.closePath()}}!function(){var t;o=d.offsetWidth,n=d.offsetHeight,(t=document.createElement("canvas")).id="canvas",t.style.top=0,t.style.zIndex=0,t.style.position="absolute",d.appendChild(t),t.parentElement.style.overflow="hidden",(a=document.getElementById("canvas")).width=o,a.height=n,r=a.getContext("2d");for(var e=0;e<o*l.density;e++){var i=new h;c.push(i)}s()}(),window.addEventListener("scroll",function(){e=!(document.body.scrollTop>n)},!1),window.addEventListener("resize",function(){o=d.clientWidth,n=d.clientHeight,d.height=n+"px",a.width=o,a.height=n},!1)}}(jQuery),qipao();var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="w(ﾟДﾟ)w 不要走！再看看嘛！",clearTimeout(titleTime)):(document.title="♪(^∇^*)欢迎回来！"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
+// 气泡
+function qipao() {
+    $('#page-header').circleMagic({
+        radius: 10,
+        density: .2,
+        color: 'rgba(255,255,255,.4)',
+        clearOffset: 0.99
+    });
+}! function(p) {
+    p.fn.circleMagic = function(t) {
+        var o, a, n, r, e = !0,
+            i = [],
+            d = p.extend({ color: "rgba(255,0,0,.5)", radius: 10, density: .3, clearOffset: .2 }, t),
+            l = this[0];
+
+        function c() { e = !(document.body.scrollTop > a) }
+
+        function s() { o = l.clientWidth, a = l.clientHeight, l.height = a + "px", n.width = o, n.height = a }
+
+        function h() {
+            if (e)
+                for (var t in r.clearRect(0, 0, o, a), i) i[t].draw();
+            requestAnimationFrame(h)
+        }
+
+        function f() {
+            var t = this;
+
+            function e() { t.pos.x = Math.random() * o, t.pos.y = a + 100 * Math.random(), t.alpha = .1 + Math.random() * d.clearOffset, t.scale = .1 + .3 * Math.random(), t.speed = Math.random(), "random" === d.color ? t.color = "rgba(" + Math.floor(255 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.floor(0 * Math.random()) + ", " + Math.random().toPrecision(2) + ")" : t.color = d.color }
+            t.pos = {}, e(), this.draw = function() { t.alpha <= 0 && e(), t.pos.y -= t.speed, t.alpha -= 5e-4, r.beginPath(), r.arc(t.pos.x, t.pos.y, t.scale * d.radius, 0, 2 * Math.PI, !1), r.fillStyle = t.color, r.fill(), r.closePath() }
+        }! function() {
+            o = l.offsetWidth, a = l.offsetHeight,
+                function() {
+                    var t = document.createElement("canvas");
+                    t.id = "canvas", t.style.top = 0, t.style.zIndex = 0, t.style.position = "absolute", l.appendChild(t), t.parentElement.style.overflow = "hidden"
+                }(), (n = document.getElementById("canvas")).width = o, n.height = a, r = n.getContext("2d");
+            for (var t = 0; t < o * d.density; t++) {
+                var e = new f;
+                i.push(e)
+            }
+            h()
+        }(), window.addEventListener("scroll", c, !1), window.addEventListener("resize", s, !1)
+    }
+}(jQuery);
+
+// 调用气泡方法
+qipao();
+
+
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
